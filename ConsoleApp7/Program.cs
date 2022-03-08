2e88f016cb920babd99fae16546c80e539a796fe
KenanNagiyev <788lnmp@code.edu.az>
2022-03-08 21:23:33 +0400

    new file

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 83, 96, 56, 2, 9, 8, 15, 62 };
+            Console.WriteLine(Min(array));
+        }
+        static int Min(int[] array)
+        {
+            int min = array[0];
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] < min)
+                {
+                    min = array[i];
+                }
+            }
+            return min;
+        }
+    }
+}
